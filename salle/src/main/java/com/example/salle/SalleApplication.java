a4d95d26311b4a5832ff3931bb615105cfa1985f
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-02 11:10:05 +0900

    ReAdd aws.properties & gitignored

--- salle/src/main/java/com/example/salle/SalleApplication.java
+++ salle/src/main/java/com/example/salle/SalleApplication.java
@@ -14,8 +14,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
-import com.example.demo.DemoApplication;
-
 @SpringBootApplication
 public class SalleApplication extends SpringBootServletInitializer {
 
