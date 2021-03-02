0ac5271fc1b080721668d9d6b38029098f848fcf
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-02 18:40:02 +0900

    simplebrokermessagehandler error

--- salle/src/main/java/com/example/salle/application/WebsocketBrokerConfig.java
+++ salle/src/main/java/com/example/salle/application/WebsocketBrokerConfig.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -36,33 +35,6 @@ public class WebsocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
 		registry.addEndpoint("/broadcast")
 			.withSockJS()
 			.setHeartbeatTime(60_000);
-//
-//		//when browser or network cause problems fallback allows realtime connectivity 
-//		registry.addEndpoint("/broadcast")
-//				.withSockJS()
-//				.setHeartbeatTime(60_000);
 	}
-
-//	@Override
-//	public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-//		
-//		DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
-//		resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
-//		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//		converter.setObjectMapper(new ObjectMapper());
-//		converter.setContentTypeResolver(resolver);
-//		messageConverters.add(converter);
-//		
-//		return false;
-//	}
-
-
-	
-	
-	
-	
-
-	
-	
 
 }
