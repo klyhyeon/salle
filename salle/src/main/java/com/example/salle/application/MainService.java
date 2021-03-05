b88d0a0ce1e361a3073f0bdf6fb3f1e95db6a27d
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-05 13:36:33 +0900

    Add @Service Annotation in MainService

--- salle/src/main/java/com/example/salle/application/MainService.java
+++ salle/src/main/java/com/example/salle/application/MainService.java
@@ -2,9 +2,11 @@ package com.example.salle.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 
+@Service
 public class MainService {
 	
 	@Autowired
