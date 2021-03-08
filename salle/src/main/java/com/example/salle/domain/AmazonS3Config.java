7555c672c8f688d2ad9d522af6f0f5aa7017d396
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-08 14:53:10 +0900

    Testing

--- salle/src/main/java/com/example/salle/domain/AmazonS3Config.java
+++ salle/src/main/java/com/example/salle/domain/AmazonS3Config.java
@@ -13,7 +13,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 public class AmazonS3Config {
 	
 
-	@Value("${cloud.aws.region.static}")
+	@Value("${cloud.aws.s3.bucket}")
 	private String region;
 	
 	@Bean
