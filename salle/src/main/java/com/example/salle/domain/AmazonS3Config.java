495f000f1568aaa7b4013773448c57ce5822e72e
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-08 18:04:45 +0900

    Deleted '/'

--- salle/src/main/java/com/example/salle/domain/AmazonS3Config.java
+++ salle/src/main/java/com/example/salle/domain/AmazonS3Config.java
@@ -3,7 +3,6 @@ package com.example.salle.domain;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
