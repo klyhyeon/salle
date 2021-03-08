7546f6408fe5b5aeb2c693b77c2be2a8bf818a81
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-08 16:01:23 +0900

    Testing without gitignore
    - Deleted credentials temporarily

--- salle/src/main/java/com/example/salle/domain/AmazonS3Config.java
+++ salle/src/main/java/com/example/salle/domain/AmazonS3Config.java
@@ -2,29 +2,23 @@ package com.example.salle.domain;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-@Configuration
-public class AmazonS3Config {
-	
 
-	@Value("${cloud.aws.credentials.accessKey}")
-	private String accessKey;
-	
-	@Value("${cloud.aws.credentials.secretKey}")
-	private String secretKey;
+@Component
+public class AmazonS3Config {
 	
 	@Value("${cloud.aws.region.static}")
 	private String region;
 	
 	@Bean
 	public BasicAWSCredentials basicAWSCredentials() {
-		return new BasicAWSCredentials(accessKey, secretKey);
+		return new BasicAWSCredentials("", "");
 	}
 	
 	@Bean
