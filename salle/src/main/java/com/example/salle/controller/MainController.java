f2e24c7cea6b7c32058403257a3be2bc6a33d314
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-09 14:47:22 +0900

    Testing RestController

--- salle/src/main/java/com/example/salle/controller/MainController.java
+++ salle/src/main/java/com/example/salle/controller/MainController.java
@@ -1,13 +1,13 @@
 package com.example.salle.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.salle.application.ProductService;
 
-@Controller
+@RestController
 public class MainController {
 	
 	@Autowired
