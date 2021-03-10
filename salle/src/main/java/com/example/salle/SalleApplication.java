74b55f4283a499170bbf3a1c724780b7d83b3368
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-10 16:57:32 +0900

    Make Git Public
    - make db.properties and set password private data.
    - covered with gitignore

--- salle/src/main/java/com/example/salle/SalleApplication.java
+++ salle/src/main/java/com/example/salle/SalleApplication.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -11,29 +12,28 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class SalleApplication extends SpringBootServletInitializer {
-	
-    public static void main(String[] args) {
-    	SpringApplication.run(SalleApplication.class,args);
-    }
 
-//    
-//	@Bean
-//	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-//		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
-//		sessionFactoryBean.setDataSource(dataSource);
-//		return sessionFactoryBean.getObject();
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(SalleApplication.class, args);
+	}
+
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		builder.sources(SpringApplication.class)
+				.properties("spring.config.location= classpath:application.properties, classpath:db.properties");
+				return builder;
+	}
+
+
+
 	@Bean
 	public MessageSource messageSource() {
 		Locale.setDefault(Locale.KOREA);
-		ResourceBundleMessageSource messageSource =
-				new ResourceBundleMessageSource();
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename("label/messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
 	}
-
-	
 
 }
