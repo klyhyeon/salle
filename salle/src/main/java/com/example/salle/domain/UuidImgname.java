6da12c020563f2cb6c6015417c9072120edc4542
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-15 15:52:24 +0900

    Uuid Reset and ProductEdit button

--- salle/src/main/java/com/example/salle/domain/UuidImgname.java
+++ salle/src/main/java/com/example/salle/domain/UuidImgname.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class UuidImgname {
 	
-	UUID uid = UUID.randomUUID();
 	
 	public String makeFilename(String filename) {
+		UUID uid = UUID.randomUUID();
 		
 		String newFilename = uid + "_" + filename; 
 		
