b27b22bd1ebe8f27a06efd74cce0597632b7757e
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-25 17:02:17 +0900

    Add @Configuration Annotation

--- salle/src/main/java/com/salle/domain/JasyptConfig.java
+++ salle/src/main/java/com/salle/domain/JasyptConfig.java
@@ -4,10 +4,12 @@ import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class JasyptConfig {
 	
-	@Bean("jasyptStringEncryptor")
+	@Bean(name="jasyptStringEncryptor")
 	public StringEncryptor stringEncrptor() {
 		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
