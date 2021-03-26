715cbf6c3003f2d8c4b27314cec75b534ef7324a
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-26 11:59:08 +0900

    Update datasource.pwd After jasypt depend. update

--- salle/src/test/java/com/salle/domain/JasyptConfigTest.java
+++ salle/src/test/java/com/salle/domain/JasyptConfigTest.java
@@ -1,9 +1,21 @@
 package com.salle.domain;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.junit.jupiter.api.Test;
+
 public class JasyptConfigTest {
+	
+	@Test
+	public void basicJasypt() {
+		BasicTextEncryptor encryptor = new BasicTextEncryptor();
+		encryptor.setPassword("salle");
+		String encrypted = "ENC(6+3bTmgFUiPZXXp2fu8y9m8/qIvyaOk1ysJKkdeP3f/9QtPsjJJSaZmUrkzWN6aD)";
+		String encryptedTmp = encryptor.encrypt("salle067138");
+		assertEquals("salle067138", encryptor.decrypt(encrypted));
+	}
+	
 	
 
 }
