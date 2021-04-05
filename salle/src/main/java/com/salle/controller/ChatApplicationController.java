f38a5a51912888a1b7a86c6f524b474ae8b8883d
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-04-05 16:08:18 +0900

    Edit Pathvariable pr_idStr

--- salle/src/main/java/com/salle/controller/ChatApplicationController.java
+++ salle/src/main/java/com/salle/controller/ChatApplicationController.java
@@ -41,7 +41,8 @@ public class ChatApplicationController {
 
 	@RequestMapping(value="/product/chatStart/{pr_id}", method=RequestMethod.GET)
 	public String productChatMessage(Model model, HttpSession session, 
-			@PathVariable("pr_id") int pr_id) throws IOException {
+			@PathVariable("pr_id") String pr_id) throws IOException {
+		int pr_id = Integer.parseInt(pr_id);
 		ChatMessage chatMessage = new ChatMessage(pr_id);
 		List<ChatMessage> chatHistory = new ArrayList<ChatMessage>();
 		ChatMessage chatMessageInfo = chatService.infoSetting(session, chatMessage, 
